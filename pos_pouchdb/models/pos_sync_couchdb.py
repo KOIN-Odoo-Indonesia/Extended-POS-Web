bafa611f8a11407190eb5b7f836b76d25f47afc7
Wahyu Hidayat <wahhid@gmail.com>
2020-07-16 21:52:59 +0700

    Add Pos Order Using create_from_ui

--- pos_pouchdb/models/pos_sync_couchdb.py
+++ pos_pouchdb/models/pos_sync_couchdb.py
@@ -10,6 +10,12 @@ _logger = logging.getLogger(__name__)
 class PosSyncCouchDB(models.Model):
     _name = 'pos.sync.couchdb'
 
+    def get_random_string(self, length):
+        letters = string.ascii_lowercase
+        result_str = ''.join(random.choice(letters) for i in range(length))
+        print("Random string of length", length, "is:", result_str)
+        return result_str
+    
     def sync(self):
         couch = couchdb.Server('http://admin:admin123@128.199.175.43:5984')
         db = couch['posorders']
@@ -17,7 +23,9 @@ class PosSyncCouchDB(models.Model):
         for row in db.view('_all_docs', include_docs=True, limit=1):
             _logger.info(row['doc'])
             #orders.append(row['doc'])
-            self.env['pos.order'].create_from_ui([{'data':row['doc']}])
+            data = row['doc']
+            data['name'] = self.get_random_string(10)
+            self.env['pos.order'].create_from_ui([{'data':data}])
         _logger.info(orders)   
             
 
